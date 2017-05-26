2a0ee633f6152d8dde911e0882a9d1b570e61fa8
Student7 <Student7@10.8.33.144>
2017-05-26 13:18:57 -0400

    Matthew 12334

--- BrooklynTechRPG/src/screens/DescriptionTextLabel.java
+++ BrooklynTechRPG/src/screens/DescriptionTextLabel.java
@@ -6,19 +6,26 @@ import java.awt.Graphics2D;
 import guiTeacher.components.TextLabel;
 
 public class DescriptionTextLabel extends TextLabel {
-
+	
+	private int width;
+	
 	public DescriptionTextLabel(int x, int y, int w, int h, String text) {
 		super(x, y, w, h, text);
-		// TODO Auto-generated constructor stub
+		this.width = w;
 	}
 	public void drawString(Graphics2D g, FontMetrics fm){
 		String s = getText();
-		int start;
+		//int x = (getWidth()/fm.stringWidth(s.substring(0,1)));
+		int start = 0;
+		int end = width/fm.stringWidth(s.substring(0,1));
 		int line = 1;
-		int x = (getWidth()-fm.stringWidth(s))/2;
 		
 		
 		
-		g.drawString(getText(), 0, (fm.getHeight()*line));
+		while(end<fm.stringWidth(s)){
+			g.drawString(s.substring(start,end), 0, (fm.getHeight()*line));
+			line++;
+		}
+		g.drawString(s.substring(start,end), 0, (fm.getHeight()*line));
 	}
 }
