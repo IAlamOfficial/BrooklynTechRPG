b5916594b428bc19e386f0c505759ecce8812d0e
Student6 <Student6@10.8.33.108>
2017-06-01 13:01:26 -0400

    Help

--- BrooklynTechRPG/src/screens/HelpScreen.java
+++ BrooklynTechRPG/src/screens/HelpScreen.java
@@ -26,7 +26,9 @@ public class HelpScreen extends FullFunctionScreen implements Runnable, KeyListe
 	private Graphic rectangleBlur;
 	private TextLabel title;
 	private CustomButton menuScreen;
-	private TextArea description;
+	private TextArea description1;
+	private TextArea description2;
+	private TextArea description3;
 	
 	public HelpScreen(int width, int height) {
 		super(width, height);
@@ -39,7 +41,7 @@ public class HelpScreen extends FullFunctionScreen implements Runnable, KeyListe
 		bitTech.setX((getWidth()-bitTech.getWidth())/2);
 		viewObjects.add(bitTech);
 		
-		rectangleBlur = new Graphic(0,300,"resources/RectangleBlur.png");
+		rectangleBlur = new Graphic(0,200,"resources/RectangleBlur.png");
 		rectangleBlur.setX((getWidth()-rectangleBlur.getWidth())/2);
 		viewObjects.add(rectangleBlur);
 		
@@ -61,7 +63,7 @@ public class HelpScreen extends FullFunctionScreen implements Runnable, KeyListe
 		
 		
 		
-		menuScreen = new CustomButton(100, 150, 200, 100, "Return", new Action() {
+		menuScreen = new CustomButton(100, 100, 200, 100, "Return", new Action() {
 			@Override
 			public void act() {
 				TechGame.trpg.setScreen(TechGame.menuScreen);
@@ -83,15 +85,15 @@ public class HelpScreen extends FullFunctionScreen implements Runnable, KeyListe
 		viewObjects.add(menuScreen);
 		
 		
-		description = new TextArea(125, 320, 750, 400, "In this RPG, there are many items and stats that will help progress you through the game.  Stats can raise or fall and items can be bought or used.");
-		description.setBorderThickness(3);
-		description.showBorder(true);
+		description1 = new TextArea(125, 205, 750, 60, "In this RPG, there are many items and stats that will help progress you through the game.  Stats can raise or fall and items can be bought or used.");
+		description1.setBorderThickness(3);
+		description1.showBorder(true);
 		try {
 			File fontFile = new File("resources/MyGirlIsRetroDEMO.ttf");
 			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
 			Font baseFont=font.deriveFont(20f);
 		//	StyledComponent.setBaseFont(baseFont);//Changes font everywhere
-			description.setFont(baseFont);
+			description1.setFont(baseFont);
 		} catch (FontFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -99,8 +101,59 @@ public class HelpScreen extends FullFunctionScreen implements Runnable, KeyListe
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		viewObjects.add(description);
+		viewObjects.add(description1);
 		
+		
+		description2 = new TextArea(125, 265, 750, 140, "Items are used to raise your stats but can cost money: \n"
+				+ "Homework Assignments :  Knowledge\nAlarm Clock :  Time\nStudy Guides :  Knowledge\nCoffee :  Time, Energy\nEnergy Drink :  Strength, Energy");
+		description2.setBorderThickness(3);
+		description2.showBorder(true);
+		try {
+			File fontFile = new File("resources/MyGirlIsRetroDEMO.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+			Font baseFont=font.deriveFont(15f);
+		//	StyledComponent.setBaseFont(baseFont);//Changes font everywhere
+			description2.setFont(baseFont);
+		} catch (FontFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		viewObjects.add(description2);
+		
+		description3 = new TextArea(125, 405, 750, 335, "Stats are what affect your interaction with others and are gained or lost doing different things:\n"
+				+ "Knowledge :  Points needed to graduate\n"
+				+ "     Gained by :  Homework, Extracurriculars                                                 Lost by :  Cutting class\n"
+				+ "Money :  Used to purchase items\n"
+				+ "     Gained by :  Daily Allowance, Defeating Bullies                              Lost by :  Homework Answers, Food, Test Answers\n"
+				+ "Reputation :  Your standing with the faculty of the school\n"
+				+ "     Gained by :  Going to class, Completing homework             Lost by :  Fighting Bullies\n"
+				+ "Energy :  Used to complete any tasks\n"
+				+ "     Gained by :  Sleeping, Eating, Completing homework         Lost by :  Attacking bullies\n"
+				+ "Strength :  Used to take on bullies\n"
+				+ "     Gained by :  Playing Sports                                                        Lost by :  Fighting Bullies\n"
+				+ "Social :  Affects prices of favors and how you’re seen by others\n"
+				+ "     Gained by :  Fighting bullies                                                      Lost by :  Inactive socially(media)\n"
+				+ "Time :  Used to limit the actions of the player but also required for the player to do tasks\n"
+				+ "     Gained by :  Coffee, Alarm clock                                         Lost by :  Doing Homework, Going to class, Completing tests");
+		description3.setBorderThickness(3);
+		description3.showBorder(true);
+		try {
+			File fontFile = new File("resources/MyGirlIsRetroDEMO.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+			Font baseFont=font.deriveFont(15f);
+		//	StyledComponent.setBaseFont(baseFont);//Changes font everywhere
+			description3.setFont(baseFont);
+		} catch (FontFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		viewObjects.add(description3);
 	}
 
 	@Override
