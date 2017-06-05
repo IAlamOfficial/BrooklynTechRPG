ee615fbe566b3200745995ca334bf1e90a467e67
Student6 <Student6@10.8.33.108>
2017-06-05 12:23:47 -0400

    Run

--- BrooklynTechRPG/src/screens/HallwayScreen.java
+++ BrooklynTechRPG/src/screens/HallwayScreen.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import TechRPG.TechGame;
+import guiTeacher.components.Action;
 import guiTeacher.components.Graphic;
 import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
@@ -14,10 +16,13 @@ import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class HallwayScreen extends FullFunctionScreen implements Runnable, KeyListener {
 	
-	private Graphic hallwaybg;
+	private Graphic hallwaybg;//screen 0
+	private Graphic classroombg;//screen 1
+	private int currentScreen = -1;
 	private int numberOfEntries = 0;
 	private TextArea intro;
-
+	private CustomButton menuScreen;
+	
 	public HallwayScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -51,7 +56,36 @@ public class HallwayScreen extends FullFunctionScreen implements Runnable, KeyLi
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		viewObjects.add(intro);
+		if(numberOfEntries == 0){
+			viewObjects.add(intro);
+			numberOfEntries++;
+		}
+		
+		classroombg = new Graphic(0,0,"resources/classroom.png");
+		classroombg.setX((getWidth()-classroombg.getWidth())/2);
+		viewObjects.add(classroombg);
+		
+		
+		menuScreen = new CustomButton(250, 60, 100, 30, "Return", new Action() {
+			@Override
+			public void act() {
+				TechGame.trpg.setScreen(TechGame.menuScreen);
+			}
+		}); 
+				try {
+			File fontFile = new File("resources/MyGirlIsRetroDEMO.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+			Font baseFont=font.deriveFont(20f);
+		//	StyledComponent.setBaseFont(baseFont);//Changes font everywhere
+			menuScreen.setFont(baseFont);
+		} catch (FontFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		viewObjects.add(menuScreen);
 	}
 
 }
