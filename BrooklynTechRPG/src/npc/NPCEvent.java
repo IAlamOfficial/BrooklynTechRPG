ee60f891c3ce22250fb3c0a0604ed5b976feb4be
Illham <Illham@Homepc>
2017-06-15 13:03:47 -0400

    updated text

--- BrooklynTechRPG/src/npc/NPCEvent.java
+++ BrooklynTechRPG/src/npc/NPCEvent.java
@@ -4,6 +4,9 @@ import guiTeacher.components.Action;
 import guiTeacher.interfaces.TextComponent;
 import items.Item;
 import player.Player;
+import screens.ClassroomScreen;
+import screens.HallwayScreen;
+import screens.HomeScreen;
 
 public class NPCEvent implements Runnable{
 	private NPC npc;
@@ -28,15 +31,14 @@ public class NPCEvent implements Runnable{
 	
 	@Override
 	public void run() {
-		this.target.setText("You just ran into "+ npc.getName()+". What would you like to do?");
-		
+		//textBox.setText
+		String[] arr = {"You just ran into "+ npc.getName()+". What would you like to do?"};
+		showTextHome(arr, null);
+		showTextClass(arr, null);
+		showTextHall(arr, null);
 	}
 	
-	public NPC getNPC() {
-		return npc;
-	}
-	
-	public void showText(String[] arr, Action a){
+	private void showTextHome(String[] arr, Action a) {
 		if( a != null){
 			a.act();
 		}
@@ -45,7 +47,54 @@ public class NPCEvent implements Runnable{
 			@Override
 			public void run() {
 				for(int i = 0; i<arr.length; i++){
-					NPCEvent.this.target.setText(arr[i]);
+					HomeScreen.textBox.setText(arr[i]);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		text.start();
+		
+	}
+
+	private void showTextClass(String[] arr, Action a) {
+		if( a != null){
+			a.act();
+		}
+		Thread text = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for(int i = 0; i<arr.length; i++){
+					ClassroomScreen.textBox.setText(arr[i]);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		text.start();
+		
+	}
+
+	
+	public void showTextHall(String[] arr, Action a){
+		if( a != null){
+			a.act();
+		}
+		Thread text = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for(int i = 0; i<arr.length; i++){
+					HallwayScreen.textBox.setText(arr[i]);
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -58,11 +107,15 @@ public class NPCEvent implements Runnable{
 		text.start();
 	}
 	
+	public NPC getNPC() {
+		return npc;
+	}
+	
 	public void beatBully(boolean result) {
 
 		if(result){
 			String[] arr = {"You beat the bully!","You earned $5.00", "You have a total of $" + p.getMoney()};
-			showText(arr, new Action() {
+			showTextHall(arr, new Action() {
 				
 				@Override
 				public void act() {
@@ -74,7 +127,7 @@ public class NPCEvent implements Runnable{
 		}
 		else{
 			String[] arr = {"You lost to the bully!","You lost $5.00", "You have a total of $" + p.getMoney()};
-			showText(arr, new Action() {
+			showTextHall(arr, new Action() {
 				
 				@Override
 				public void act() {
@@ -89,14 +142,14 @@ public class NPCEvent implements Runnable{
 	
 	public void couldNotFight() {
 		String[] arr = {"Sorry you do not have enough energy to fight the bully"};
-		showText(arr, null);
+		showTextHall(arr, null);
 		
 	}
 	
 	public void purchaseItem(Item item){
 		i = item;
 		String[] arr = {"You have purchased " + i.getName() + " for $" + (i.getCost()*p.getSocial()), "You have a total of $" + p.getMoney()};
-		showText(arr, new Action() {
+		showTextHall(arr, new Action() {
 			
 			@Override
 			public void act() {
@@ -109,14 +162,14 @@ public class NPCEvent implements Runnable{
 	
 	public void couldNotPpurchase() {
 		String[] arr = {"Sorry you do not have enough money to purchase this item"};
-		showText(arr, null);
+		showTextHall(arr, null);
 		
 	}
 	
 	public void returnHomework(boolean result) {
 		if(!result){
 			String[] arr = {"You gave in the homework","You have recived 5 Knowledge points", "You have a total of " + p.getKnowledgePoints() + " knowledge points"};
-			showText(arr, new Action() {
+			showTextClass(arr, new Action() {
 
 				@Override
 				public void act() {
@@ -129,7 +182,7 @@ public class NPCEvent implements Runnable{
 			});
 		}else{
 			String[] arr = {"You got caught using someone else homework","You have lost 25 Knowledge points", "You have a total of " + p.getKnowledgePoints() + " knowledge points"};
-			showText(arr, new Action() {
+			showTextClass(arr, new Action() {
 
 				@Override
 				public void act() {
@@ -145,12 +198,12 @@ public class NPCEvent implements Runnable{
 
 	public void haveNoHomework(){
 		String[] arr = {"You have no homework to give"};
-		showText(arr, null);
+		showTextClass(arr, null);
 	}
 	
 	public void doMath(Player p) {
 		String[] arr = {"You have completed a homework assigment.","You have " + p.getHw() + " completed assigments"};
-		showText(arr, new Action() {
+		showTextHome(arr, new Action() {
 
 			@Override
 			public void act() {
@@ -165,13 +218,13 @@ public class NPCEvent implements Runnable{
 	
 	public void dontDoMath(String string) {
 		String[] arr = {"You do not have enough " + string + " to complete your homweork"};
-		showText(arr, null);
+		showTextHome(arr, null);
 		
 	}
 	
 	public void socialMedia(Player p) {
 		String[] arr = {"You have went online with friends", "You have gained a better reputation"};
-		showText(arr, new Action() {
+		showTextHome(arr, new Action() {
 
 			@Override
 			public void act() {
@@ -184,13 +237,13 @@ public class NPCEvent implements Runnable{
 	
 	public void dontDoSocialMedia(String string) {
 		String[] arr = {"You do not have enough " + string + " to go online"};
-		showText(arr, null);
+		showTextHome(arr, null);
 		
 	}
 	
 	public void play(Player p) {
 		String[] arr = {"You have went outside to play", "You have gained more strength"};
-		showText(arr, new Action() {
+		showTextHall(arr, new Action() {
 
 			@Override
 			public void act() {
@@ -205,13 +258,13 @@ public class NPCEvent implements Runnable{
 	
 	public void dontPlay(String string) {
 		String[] arr = {"You do not have enough " + string + " to play outside"};
-		showText(arr, null);
+		showTextHall(arr, null);
 		
 	}
 	
 	public void learn(Player p) {
 		String[] arr = {"You went to class and learned", "You have gained 10 knowledge points", "You have a total of " + p.getKnowledgePoints() + " knowledge points" };
-		showText(arr, new Action() {
+		showTextClass(arr, new Action() {
 
 			@Override
 			public void act() {
@@ -227,13 +280,13 @@ public class NPCEvent implements Runnable{
 	
 	public void dontLearn(String string) {
 		String[] arr = {"You do not have enough " + string + " to go to class"};
-		showText(arr, null);
+		showTextClass(arr, null);
 		
 	}
 	
 	public void eat(Player p2) {
 		String[] arr = {"You have went to eat", "You have gained more energy"};
-		showText(arr, new Action() {
+		showTextHome(arr, new Action() {
 
 			@Override
 			public void act() {
@@ -247,7 +300,7 @@ public class NPCEvent implements Runnable{
 
 	public void dontEat() {
 		String[] arr = {"You do not have enough time to go eat"};
-		showText(arr, null);
+		showTextHome(arr, null);
 		
 	}
 
